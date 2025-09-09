2e5a5e179744b36bc3d815abe06e9ce1fd3ad9d9
pavlik3229 <147932271+pavlik3229@users.noreply.github.com>
2025-09-09 14:20:43 +0300

    roulette rotation, home page and another improvements

--- webgames/games/views.py
+++ webgames/games/views.py
@@ -1,18 +1,25 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, FormView
+from .forms import RouletteBetForm
 
 # Create your views here.
 
 
 class HomePage(TemplateView):
     template_name = 'games/index.html'
-    extra_context = {
-        'title': 'Home',
-    }
 
-class Roulette(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if "balance" not in self.request.session:
+            self.request.session["balance"] = 200
+        context["balance"] = self.request.session["balance"]
+        return context
+
+
+
+class Roulette(FormView):
+    form_class = RouletteBetForm
     template_name = 'games/roulette.html'
     extra_context = {
         'title': 'Roulette',
